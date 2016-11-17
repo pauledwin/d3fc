bf5005a450a403fc20ef0926a8e6f598a37c01c6
Ali Sheehan-Dare <alisd23@live.co.uk>
2016-11-17 13:52:04 +0000

    feat: started on the stacked example

--- site/src/example-charts/stacked/stacked.js
+++ site/src/example-charts/stacked/stacked.js
@@ -6,50 +6,56 @@ d3.csv('https://d3fc.io/examples/stacked/data.csv', (error, data) => {
   const spread = fc.group()
     .key('Country');
   const stackLayout = d3.stack()
-    .values(d => d.values);
+    .keys(c => {
+      return c.map(d => d.key);
+    })
+    .value((v, key) => {
+      console.log(v, key, v[key]);
+      return v;
+    });
 
   const series = stackLayout(spread(data));
 
-  const color = d3.schemeCategory20()
-    .domain(series.map(s => s.key));
+  const color = d3.schemeCategory20;
+    // .domain(series.map(s => s.key));
 
   const yExtent = fc.extentLinear()
     .include(0)
-    .fields([a => a.map(d => d.y + d.y0)]);
+    .accessors([a => a.map(d => d.y + d.y0)]);
 
-  const legend = d3.legend.color()
+  const legend = d3.legendColor()
     .orient('horizontal')
     .shapeWidth(70)
-    .scale(color);
+    .scale(d3.scaleOrdinal(color));
 
-  const stack = fc.series.stacked.bar()
+  const stack = fc.seriesSvgBar()
     .orient('horizontal')
-    .yValue(function(d) { return d.x; })
-    .xValue(function(d) { return d.y0 + d.y; })
-    .x0Value(function(d) { return d.y0; })
-    .decorate(function(sel, _, index) {
+    .mainValue(d => d.x)
+    .crossValue(d => d.y0 + d.y)
+    // .x0Value(d => d.y0)
+    .decorate((sel, _, index) => {
       sel.enter().attr('fill', color(series[index].key));
     });
 
-  const chart = fc.chart.cartesian(
-    d3.scale.linear(),
-    d3.scale.ordinal()
+  const chart = fc.chartSvgCartesian(
+    d3.scaleLinear(),
+    d3.scaleOrdinal()
   )
-    .xDomain(yExtent(series.map(function(d) { return d.values; })))
-    .yDomain(data.map(function(d) { return d.Country; }))
+    .xDomain(yExtent(series.map(d => d.values)))
+    .yDomain(data.map(d => d.Country))
     .xLabel('(million tonnes of oil equivalent)')
     .xNice()
     .chartLabel('2013 Energy Production')
     .yOrient('left')
-    .yTickSize(0)
-    .margin({left: 100, bottom: 40, right: 10, top: 30})
+    // .yTickSize(0)
+    // .margin({left: 100, bottom: 40, right: 10, top: 30})
     .plotArea(stack)
-    .decorate(function(selection) {
+    .decorate(selection => {
       // add a container for the legend
       selection.enter()
         .append('g')
         .classed('legend-container', true)
-        .layout({
+        .style({
           position: 'absolute',
           right: 10,
           top: 40,
@@ -58,14 +64,15 @@ d3.csv('https://d3fc.io/examples/stacked/data.csv', (error, data) => {
         });
 
       // compute layout from the parent SVG
-      selection.enter().layout();
+      selection.enter();
 
       // render the legend
       selection.select('g.legend-container')
         .style('display', function() {
           // magic number because the legend accessors return
           // the legend itself instead of the values (susielu/d3-legend#23)
-          const availableWidth = selection.select('.plot-area').layout('width');
+          const availableWidth = selection.select('.plot-area')
+          // .layout('width');
           return availableWidth > (75 + 5) * data.length ? '' : 'none';
         })
         .call(legend);
